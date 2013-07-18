421796442820925c677c5bc5fe4b33dc4c78ee4f
UlickNormanOwen <ryoyahp@gmail.com>
2013-07-18 23:17:59 +0900

    update_1

--- TimeLocale.java
+++ TimeLocale.java
@@ -10,8 +10,12 @@
 			long utime = System.currentTimeMillis ();
 			System.out.println ( "Unix Time = " + utime );
 			
-			//	US側の時間取得
+			//	US側の時間取得。Format型の変数に、DateFormat.getDateTimeInstanceを利用して代入する
+			//	コレを利用すると、ロケールがUS、書式もUS形式で表示される
 			Format fmt = DateFormat.getDateTimeInstance ( DateFormat.LONG, DateFormat.FULL, Locale.US );
-			System.out.println ( fmt.format( new Date () ) );
+			//	今の時刻取得
+			Date now = new Date ();
+			
+			System.out.println ( fmt.format( now ) );
 		}
 	}
